e7e7cf12af64384ddfe8e0c436db9666bf0415aa
G-0324D <byesad@gmail.com>
2022-08-31 00:23:26 +0300

    Create - operatorler
    Temel matematik ifadeleri ve koşul örneği.

---
+++ operatorler.swift
@@ -0,0 +1,30 @@
+let x = 10 // Eşittir Operatörü
+var y = 20
+y = x // x içindeki değeri y değerine ata
+
+print(x,y)
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y) // Mod alma - Bölümden kalan
+
+var a = 5
+a += 2 // Şu şekilde de yazılabilir a = a + 2 -> a++ diye bir tanım Swift de yok
+print(a)
+
+
+a == x
+a != x // Eşit değil
+a <= x 
+a >= x
+a < x
+
+x ? y : z // x Boole yani true yada  false bir değer olmalı
+// : eğer anlamına gelebilir
+
+"x" == "x" ? print("Eşitlik doğru") : print("Eşitlik yanlış") // x, x'e eşit mi? Sonuç true yani doğru ise 1. mesajı yazdır : yani değilse 2. mesajı yazdır. if koşuluna benze.
+
+
+
