2a3d5a50437006224d32740357b6fb7ea04c61e5
osmanesad <byesad@gmail.com>
2022-09-01 21:35:40 +0300

    Created - UILabel Project
    Bir UILabel çalışma alanı oluşturduk. UILbel'ı tanıyoruz.

---
+++ UILabel/UILabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by Osman Esad on 1.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
