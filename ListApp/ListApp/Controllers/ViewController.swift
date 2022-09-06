af6c1ccabffd6aa69bf8b7b2f8e8e7f4aa5e5d58
osmanesad <byesad@gmail.com>
2022-09-07 02:58:31 +0300

    Upadte - UIAlertController
    Listeye eleman ekleme işlemini gerçekleştirdik. UIAlertController ile ekrana çıkan mesaj kutusundan veri girişi ve kontrolleri yapıldı.

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-
+    
+    
     @IBOutlet weak var tableView: UITableView!
-    let exampleData = ["Kitap","Kahve","Kırtasiye"]
+    
+    var exampleData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +26,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
         
         cell.textLabel?.text = exampleData[indexPath.row]
@@ -33,8 +34,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    @IBAction func didAddBarButtonItemTapped(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "Yeni içerik eklenecek.", message: nil, preferredStyle: .alert)
+        
+        
+        
+        let defaultButton = UIAlertAction(title: "Ekle", style: .default) { _ in
+            let text = alertController.textFields?.first?.text
+            
+            if text != "" {
+                self.exampleData.append((text)!)
+                self.tableView.reloadData()
+                
+            } else {
+                
+                let alertController = UIAlertController(title: "Uyarı", message: "Listeye boş eleman eklenemez", preferredStyle: .alert)
+                
+                let cancelButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+                
+                alertController.addAction(cancelButton)
+                self.present(alertController, animated: true)
+                
+                
+                
+            }
+            
+        }
+        
+        let cancelButton = UIAlertAction(title: "İptal", style: .cancel, handler: nil)
+        
+        
+        
+        alertController.addTextField()
+        
+        alertController.addAction(defaultButton)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true)
+        
+        
+    }
     
-
-
+    
+    
 }
 
