46a6922ba08c8544f029b422e5d18092b6c04d5c
osmanesad <byesad@gmail.com>
2022-09-06 19:58:00 +0300

    Created - ListApp
    Bir listeleme uygulaması yapıyoruz. Bunu için gerekli ayarlamaları yaptık.

---
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Osman Esad on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
