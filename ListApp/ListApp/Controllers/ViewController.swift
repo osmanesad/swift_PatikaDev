486807ee422a8a13c53b272035cccdd9a5d9cc5e
osmanesad <byesad@gmail.com>
2022-09-06 20:42:36 +0300

    Update - TableVaiew
    Bir tablo oluşturduk. Tablomuzun kod bağlantılarını yaptık. Örnek bir data oluşturduk ve listedik.

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    let exampleData = ["Kitap","Kahve","Kırtasiye"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        
+        cell.textLabel?.text = exampleData[indexPath.row]
+        
+        return cell
+    }
+    
+    
 
 
 }
