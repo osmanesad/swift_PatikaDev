4ffc85aa8bf348fa746daddbad18a74f1ede5736
osmanesad <byesad@gmail.com>
2022-09-06 19:39:47 +0300

    Created - Stack and Image View
    Ekrana yerleştirdiğimiz görsellerin konumlarını ayarladığımız bir çalışma. Farklı ekranlarda uyumlu çalışması için ayarlar yağıldı.

---
+++ UIStackView_ImageView/UIStackView_ImageView/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIStackView_ImageView
+//
+//  Created by Osman Esad on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
