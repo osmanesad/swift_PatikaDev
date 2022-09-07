a3962984748eb59d2cd75010afc2c4b440197f41
osmanesad <byesad@gmail.com>
2022-09-08 01:30:50 +0300

    Update - Add and delete data
    Listeye veri ekleme ekranını geliştirdik. Ekrana gelen mesaj ekranın fonksiyonlarını düzenledik. Listedeki tüm elemanları silme işlemini gerçekleştirdik.

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+    var alertController = UIAlertController()
     @IBOutlet weak var tableView: UITableView!
-    
     var exampleData = [String]()
     
     override func viewDidLoad() {
@@ -26,56 +25,72 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        
         cell.textLabel?.text = exampleData[indexPath.row]
-        
         return cell
     }
     
-    @IBAction func didAddBarButtonItemTapped(_ sender: UIBarButtonItem) {
+    @IBAction func didRemoveBarButtonItemTapped(_ sender: UIBarButtonItem) {
         
-        let alertController = UIAlertController(title: "Yeni içerik eklenecek.", message: nil, preferredStyle: .alert)
-        
-        
-        
-        let defaultButton = UIAlertAction(title: "Ekle", style: .default) { _ in
-            let text = alertController.textFields?.first?.text
-            
-            if text != "" {
-                self.exampleData.append((text)!)
-                self.tableView.reloadData()
-                
-            } else {
-                
-                let alertController = UIAlertController(title: "Uyarı", message: "Listeye boş eleman eklenemez", preferredStyle: .alert)
-                
-                let cancelButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
-                
-                alertController.addAction(cancelButton)
-                self.present(alertController, animated: true)
-                
-                
-                
-            }
-            
+        presentAlert(title: "Uyarı",
+                     message: "Listedeki bütün elemanlar silinicek",
+                     preferredStyle: .alert,
+                     cancelButtonTitle: "İptal",
+                     isTextFieldAvailable: false,
+                     defaultButtonTitle: "Sil") { _ in
+            self.exampleData.removeAll()
+            self.tableView.reloadData()
         }
         
-        let cancelButton = UIAlertAction(title: "İptal", style: .cancel, handler: nil)
-        
-        
-        
-        alertController.addTextField()
-        
-        alertController.addAction(defaultButton)
-        alertController.addAction(cancelButton)
-        present(alertController, animated: true)
         
         
     }
     
+    @IBAction func didAddBarButtonItemTapped(_ sender: UIBarButtonItem) {
+        presentAddAlert()
+    }
     
+    // MARK: Show Alert Dunction -
+    func presentAddAlert() {
+        presentAlert(title: "Yeni eleman ekle",
+                     message: nil,
+                     preferredStyle: .alert,
+                     cancelButtonTitle: "Vazgeç",
+                     isTextFieldAvailable: true,
+                     defaultButtonTitle: "Ekle", defaultButtonHandler: { _ in
+            let text = self.alertController.textFields?.first?.text
+            if text != "" {
+                self.exampleData.append((text)!)
+                self.tableView.reloadData()
+            } else {
+                self.presentWarningAlert()
+            }
+        })
+    }
     
+    // MARK: Warning Alert Function -
+    func presentWarningAlert() {
+        presentAlert(title: "Uyarı", message: "Listeye boş eleman eklenemez", cancelButtonTitle: "Tamam")
+    }
+    
+    func presentAlert(title: String, message: String?, preferredStyle: UIAlertController.Style = .alert, cancelButtonTitle: String?, isTextFieldAvailable: Bool = false,defaultButtonTitle: String? = nil, defaultButtonHandler: ((UIAlertAction)-> Void)? = nil) {
+        
+        alertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        
+        let cancelButton = UIAlertAction(title: cancelButtonTitle, style: .cancel, handler: nil)
+        
+        if defaultButtonTitle != nil {
+            let defaultButton = UIAlertAction(title: defaultButtonTitle, style: .default, handler: defaultButtonHandler)
+            alertController.addAction(defaultButton)
+        }
+        
+        if isTextFieldAvailable {
+            alertController.addTextField()
+        }
+        
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true)
+        
+    }
 }
 
