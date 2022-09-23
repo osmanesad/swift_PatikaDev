c927089f2e86143d1e3b184101fe582ff330c247
osmanesad <byesad@gmail.com>
2022-09-23 20:21:56 +0300

    Update
    Bir kaç deneme üzerinden öğrenmeye çalışıyoruz.

---
+++ class_struct.playground/Contents.swift
@@ -0,0 +1,11 @@
+struct StructName {
+    let someProperty: Int
+    
+    func getSomeProperty() -> Int {
+        return someProperty
+    }
+}
+
+class ClassNamae {
+    var someProperty: String = ""
+}
