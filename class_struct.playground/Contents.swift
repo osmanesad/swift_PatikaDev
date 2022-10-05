c89f863f7a3c5f17298aae67fc4761bc27e11436
osmanesad <byesad@gmail.com>
2022-10-06 00:32:17 +0300

    Update - Struct and Class
    Konuyla alakalı çalışmalar.

--- class_struct.playground/Contents.swift
+++ class_struct.playground/Contents.swift
@@ -1,6 +1,14 @@
 struct StructName {
     let someProperty: Int
     
+
+    
+    init(somePorperty: Int = 0){
+        self.someProperty = somePorperty
+    }
+}
+
+extension StructName{
     func getSomeProperty() -> Int {
         return someProperty
     }
@@ -8,4 +16,19 @@ struct StructName {
 
 class ClassNamae {
     var someProperty: String = ""
+    
+    func yourName() {
+        print("My name is \(someProperty)")
+    }
+    
+    init(someProperty: String = "ClassName"){
+        self.someProperty = someProperty
+    }
 }
+
+let className = ClassNamae(someProperty: "Osman Esad")
+print(className.yourName())
+
+let someStruct = StructName(somePorperty: 29)
+someStruct.getSomeProperty()
+print(someStruct.someProperty)
